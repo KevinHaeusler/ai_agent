fe591a0c4012a170848b377bdb3114ceddc629af
KevinHaeusler <kevin@kh.dev>
2025-12-13 14:38:24 +0100

    CH1L6: Multiple Messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,9 +11,10 @@ client = genai.Client(api_key=api_key)
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=args.user_prompt,
+    model="gemini-2.5-flash", contents=messages
 )
 if response.usage_metadata == None:
     raise RuntimeError("Usage Metadata is empty, failed API request?")
