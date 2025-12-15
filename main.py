2b4c46905790b1d8c518402c4f1e2eb5413020ba
KevinHaeusler <kevin@kh.dev>
2025-12-15 10:19:11 +0100

    CH3L2: Function Declaration

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from google import genai
 import argparse
 from google.genai import types
 from prompts import system_prompt
+from functions.get_files_info import schema_get_files_info
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,10 +16,18 @@ parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info],
+)
+
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    ),
 )
 if response.usage_metadata == None:
     raise RuntimeError("Usage Metadata is empty, failed API request?")
@@ -25,4 +35,8 @@ if args.verbose:
     print(
         f"User prompt: {args.user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
     )
-print(response.text)
+if response.function_calls:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print(response.text)
